6e9902ae3de2772afcda23fc25744a5f16691b3d
JuliarGubolin <julia.gubolin@unesp.br>
2022-05-16 14:34:21 -0300

    commit-inicial

---
+++ bbnprojeto/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Button, TextInput, Image, ImageBackground } from "react-native";
+import estilos from './styles/estilo.js'
+
+const image = { uri: "https://reactjs.org/logo-og.png" };
+
+export default function App() {
+  return (
+    <View style={estilos.container}>
+      <Image style={estilos.logo} source={require("./source/images/flor.jpg")} />
+      <TextInput style={estilos.input} placeholder="Email" placeholderTextColor="#000" />
+      <TextInput
+        style={estilos.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+        placeholderTextColor="#000"
+      />
+      <View style={{width:'100%', flexDirection:'row', justifyContent:'space-evenly'}}>
+        <Button style={estilos.botao} title="Entrar" />
+        <Button style={estilos.botao} title="Cadastre-se" />
+      </View>
+    </View>
+  );
+}
---
+++ bbnprojeto/styles/estilo.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#00000",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 100,
+      backgroundColor: '#fff'
+    },
+    input: {
+      height: 40,
+      width: 260,
+      margin: 12,
+      borderWidth: 2,
+      padding: 10,
+      borderRadius: 4,
+      borderColor: "#02a9f7",
+    },
+    logo: {
+      height: 150,
+      width: 150,
+    },
+    placeholder:{
+      color:"#000"
+    },
+    botao:{
+      borderRadius: 16,
+      margin: 24,
+    }
+  });
