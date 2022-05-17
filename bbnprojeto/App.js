75ade9e588c253332245925abd5464f75db239e2
JuliarGubolin <julia.gubolin@unesp.br>
2022-05-17 13:52:54 -0300

    atualizacao-appjs

--- bbnprojeto/App.js
+++ bbnprojeto/App.js
@@ -2,24 +2,45 @@ import React from "react";
 import { View, TextInput, Image, TouchableOpacity, Text } from "react-native";
 import estilos from './styles/estilo.js'
 
-export default function StackScreen() {
+export default function App() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    >
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ title: 'My home' }}
+    <View style={estilos.container}>
+      <Image 
+        style={estilos.logo} 
+        source={require("./source/images/Logo_Teste.jpg")} 
       />
-    </Stack.Navigator>
+      <View>
+        <Text style={estilos.titulo}>Best Bloco de Notas</Text>
+        <Text style={estilos.subtitulo}>BBN</Text>
+      </View>
+      <View style={estilos.containerLogin}>
+        <Text style={estilos.placeholder}> E-mail:</Text>
+        <TextInput 
+          style={estilos.input} 
+          placeholder="Insira seu e-mail" 
+          placeholderTextColor={'#77778D'} />
+        <Text style={estilos.placeholder}> Senha:</Text>
+        <TextInput
+          style={estilos.input}
+          placeholder="Senha"
+          placeholderTextColor={'#77778D'}
+          secureTextEntry={true}
+        />
+        <View style={{alignItems:'center'}}>
+          <TouchableOpacity>
+            <Text style={estilos.botaoEntrar}>
+            Entrar</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={estilos.viewEsqueceuSenha}>
+          <Text style={estilos.txtEsqueceuSenha}>Esqueceu a senha?</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={estilos.botaoLink}>
+        <TouchableOpacity>
+          <Text style={estilos.underLineText}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
