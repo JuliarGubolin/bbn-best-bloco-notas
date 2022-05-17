14bba87ef73d79cda587dc76eeb73d526d6a101e
JuliarGubolin <julia.gubolin@unesp.br>
2022-05-17 10:27:44 -0300

    organizacao-estilos-inicio-tela-principal

--- bbnprojeto/screens/Principal.js
+++ bbnprojeto/screens/Principal.js
@@ -1,14 +1,46 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from "react";
+import { View, TextInput, Image, TouchableOpacity, Text } from "react-native";
+import estilos from './styles/estilo.js'
 
-
-export default function Principal() {
+export default function App() {
   return (
-    <View>
-      <Text>
-        HELLO WORLD
-      </Text>
+    <View style={estilos.container}>
+      <Image 
+        style={estilos.logo} 
+        source={require("./source/images/Logo_Teste.jpg")} 
+      />
+      <View>
+        <Text style={estilos.titulo}>Best Bloco de Notas</Text>
+        <Text style={estilos.subtitulo}>BBN</Text>
+      </View>
+      <View style={estilos.containerLogin}>
+        <Text style={estilos.placeholder}> E-mail:</Text>
+        <TextInput 
+          style={estilos.input} 
+          placeholder="Insira seu e-mail" 
+          placeholderTextColor={'#77778D'} />
+        <Text style={estilos.placeholder}> Senha:</Text>
+        <TextInput
+          style={estilos.input}
+          placeholder="Senha"
+          placeholderTextColor={'#77778D'}
+          secureTextEntry={true}
+        />
+        <View style={{alignItems:'center'}}>
+          <TouchableOpacity>
+            <Text style={estilos.botaoEntrar}>
+            Entrar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={estilos.viewEsqueceuSenha}>
+          <Text style={estilos.txtEsqueceuSenha}>Esqueceu a senha?</Text>
+        </View>
+      </View>
+      <View style={estilos.botaoLink}>
+        <TouchableOpacity>
+          <Text style={estilos.underLineText}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
--- bbnprojeto/styles/estilo.js
+++ bbnprojeto/styles/estilo.js
@@ -22,7 +22,8 @@ export default StyleSheet.create({
     logo: {
       height: 150,
       width: 150,
-      borderRadius: 10
+      borderRadius: 10,
+      marginBottom: 12
     },
     placeholder:{
       color:"#000"
@@ -33,5 +34,64 @@ export default StyleSheet.create({
       color: 'blue',
       fontWeight: 'bold',
       textAlign: 'center',
+      color: '#02a9f7'
+    },
+    titulo:{
+      fontWeight: 'bold', 
+      fontSize: 28, 
+      color:'black', 
+      width: 300, 
+      textAlign:'center'
+    },
+    subtitulo:{
+      fontWeight: 'bold', 
+      fontSize: 20, 
+      color:'#77778D', 
+      textAlign:'center'
+    },
+    containerLogin:{
+      backgroundColor:'#EEEEEE', 
+      borderRadius:8, 
+      padding:12, 
+      margin:8
+    },
+    placeholder:{
+      fontWeight: 'bold', 
+      fontSize: 15, 
+      color:'black', 
+      marginLeft:12
+    },
+    botaoLink:{
+      width:'100%', 
+      flexDirection:'row', 
+      justifyContent:'space-evenly'
+    },
+    viewEsqueceuSenha:{
+      flexDirection:'row', 
+      justifyContent:'flex-end', 
+      marginTop:10, 
+      marginRight:12
+    },
+    txtEsqueceuSenha:{
+      color:'#77778D'
+    },
+    botaoEntrar:{
+      color:'white', 
+      backgroundColor:'#2196F3', 
+      textAlign:'center', 
+      height:30, 
+      width:260, 
+      textAlignVertical:'center'
+    },
+    botaoPrincipal:{
+      fontSize: 50,
+      color:'white', 
+      backgroundColor:'#2196F3', 
+      textAlign:'center', 
+      height:100, 
+      width: 300, 
+      textAlignVertical:'center',
+      borderRadius: 4,
+      margin: 8
     }
   });
