275dbf7e8f95f401bce8ac10249bbd4e026a51bd
GustavoPazzini <gustavo200699@hotmail.com>
2022-06-16 14:20:20 -0300

    tela de home

--- bbnprojeto/App.js
+++ bbnprojeto/App.js
@@ -7,6 +7,7 @@ import Home from './screens/Home';
 import Login from './screens/Login';
 import Cadastro from './screens/Cadastro';
 import EsqueceuSenha from './screens/EsqueceuSenha';
+import {Button, icon} from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +17,15 @@ export default class App extends Component{
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
           <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Home" component={Home} options={{
+            headerShown: true, //Colocar a barra de cima
+            headerTintColor:"#fff", //Cor para o Texto
+            headerStyle:{backgroundColor:"#02a9f7"}, //Cor do fundo do header
+            headerBackVisible:false, //Seta pra voltar pra trás
+            headerTitle: "Ok", //Muda a frase do título
+            headerRight: () => ( 
+              <Button icon="plus-box" style={{width:0}} onPress={() => console.log} color="#fff"/>)
+            }}/>
           <Stack.Screen name="Cadastro" component={Cadastro} />
           <Stack.Screen name="EsqueceuSenha" component={EsqueceuSenha} />
         </Stack.Navigator>
--- bbnprojeto/screens/Home.js
+++ bbnprojeto/screens/Home.js
@@ -37,23 +37,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={estilos.container}>
-        <View style={estilos.botaoHome}>
-            <View style={{flex:1, flexDirection:"row", alignItems:'center'}} >
-                <Text style={{color:'#000'}}>Best Bloco de Notas</Text>
-            </View>
-        </View>
+        
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  statusBar: {
-    backgroundColor: '#2196F3',
-    height: 50
-  },
-  container: {
-    flex: 1
-  }
-});
-
